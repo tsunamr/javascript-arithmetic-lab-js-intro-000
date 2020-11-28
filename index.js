72acab11f8cc14ae112aa45ddbb047d9f438aab3
sunam RImal Nepali <tsunamr@gmail.com>
2020-11-28 16:30:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(number){
+  return number
+}
+function subtract(number){
+  return number
+}
+function multiply(number){
+  return number
+}
+function divide(number){
+  return number
+}
+function add(a,b){
+  return (a+b)
+}
+function subtract(a,b){
+  return (a-b)
+}
+function multiply(a,b){
+  return (a*b)
+}
+function divide(a,b){
+  return (a/b)
+}
+function increment(n){
+  return ++n
+}
+function decrement(n){
+  return --n
+}
+function makeInt(n, base){
+  parseInt('n', 10)
+  return n
+}
+function preserveDecimal(n){
+  parseFloat('n', 10)
+  return n
+}
